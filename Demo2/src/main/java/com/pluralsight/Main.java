85b06daf9d9ecbc4ed9b645858bc5d879903f0c1
jec9386 <jerrychandev@gmail.com>
2025-06-12 14:27:58 -0400

    BasicDataSource

--- Demo2/src/main/java/com/pluralsight/Main.java
+++ Demo2/src/main/java/com/pluralsight/Main.java
@@ -1,14 +1,15 @@
 package com.pluralsight;
 
-import java.sql.*;
+import org.apache.commons.dbcp2.BasicDataSource;
 
+import java.sql.*;
 
 public class Main {
 
     private static BasicDataSource basicDataSource;
 
-
     public static void main(String[] args) {
+
         if (args.length != 3) {
             System.out.println(
                     "Application needs three arguments to run: " +
@@ -18,46 +19,84 @@ public class Main {
 
         basicDataSource = getBasicDataSourceFromArgs(args);
 
-        try {
+        try{
             displayCities(103);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
+
+
+
     }
 
-    //-----------------------------------------------------------------------------------------------
-
-    //extracting values from args
-    public static BasicDataSource getBasicDataSourceFromArgs(String[] args) {
+    public static BasicDataSource getBasicDataSourceFromArgs(String[] args){
+        // get the user name and password from the command line args
         String username = args[0];
         String password = args[1];
-        String connectionString = args[2];
 
-        return new BasicDataSource(connectionString, username, password);
+        String connectionString = args[2];
+        BasicDataSource result = new BasicDataSource();
+        result.setUsername(username);
+        result.setPassword(password);
+        result.setUrl(connectionString);
+
+        return result;
     }
 
-    //----------------------------------------------------------------------------------------------------
-    public static void displayCities(int countryID) throws SQLException, ClassNotFoundException {
-        //3 nested try block
-        try (Connection connection = basicDataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("SELECT city FROM city " + "WHERE country_id = ?");) {
-            ps.setInt(1, countryID);   //set parameter that query need, because we have parameter we cant consolidate this further
 
-            try (ResultSet results = ps.executeQuery()) { //3rd try to create results
-                // process the results
+    public static void displayCities(int countryId) throws SQLException, ClassNotFoundException {
+
+        try (Connection connection = basicDataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT city FROM city WHERE country_id = ?");)
+        {
+            ps.setInt(1, countryId);
+
+            try(ResultSet results = ps.executeQuery()) {
                 while (results.next()) {
                     String city = results.getString("city");
                     System.out.println(city);
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-
-            {
-                System.out.println("Error displaying cities: " + e.getMessage());
+            finally{
+                //close the results ResultSet
             }
 
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+        finally {
+            //close the ps PreparedStatement
+            //close the connection Connection
+        }
+
     }
+
+    public static void displayAllCities() throws SQLException, ClassNotFoundException {
+
+
+        try (Connection connection = basicDataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT city FROM city where country_id");
+             ResultSet results = ps.executeQuery();
+        )
+        {
+            while (results.next()) {
+                String city = results.getString("city");
+                System.out.println(city);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            //close the resultset
+            //close the prepared statement
+            //close the connection
+        }
+
+    }
+
+
 }
+
